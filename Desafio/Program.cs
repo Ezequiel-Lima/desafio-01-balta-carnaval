cad24a0b0f2ed4053fbe668fe280b4660caea190
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2023-02-17 08:18:34 -0300

    Add project files.

---
+++ Desafio/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite um texto:");
+string texto = Console.ReadLine();
+
+var (palavras, caracteres) = (texto.Split(' ').Length, texto.Length);
+
+Console.WriteLine($"{caracteres} caracteres, {palavras} palavras");
